f484c1df2cd499d85e8d063486962baf130a5ff5
Jarvis Jiang <jiang115jie@gmail.com>
2013-03-31 17:55:09 +0800

    test

--- app.js
+++ app.js
@@ -2,34 +2,30 @@
 /**
  * Module dependencies.
  */
-
+var POST = 3000;
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
+  , routes = require('./server/routes')
   , http = require('http')
   , path = require('path');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || POST);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  // app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'static')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port %d in %s mode, pid %d", app.get('port') , app.settings.env, process.pid);
 });
