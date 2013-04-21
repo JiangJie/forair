d5e8fe7711447996543f783e5dde584487872451
Jarvis Jiang <jiang115jie@gmail.com>
2013-04-22 00:11:12 +0800

    fix

--- static/jarvis/js/config.js
+++ static/jarvis/js/config.js
@@ -1,6 +1,6 @@
 requirejs.config({
   paths: {
-    jquery: 'http://code.jquery.com/jquery-git2',
+    jquery: 'http://code.jquery.com/jquery-2.0.0.min',
     bootstrap: 'http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min'
   },
   shim: {
