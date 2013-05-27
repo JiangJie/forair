672666e4fea88792e4653661739e1b29d7c44347
Jarvis Jiang <jiang115jie@gmail.com>
2013-05-28 01:11:06 +0800

    add

--- static/jarvis/js/config.js
+++ static/jarvis/js/config.js
@@ -1,10 +1,12 @@
 requirejs.config({
   paths: {
-    jquery: 'http://code.jquery.com/jquery-2.0.0.min',
+    // jquery: 'http://code.jquery.com/jquery-2.0.0.min',
+    jquery: '/jarvis/js/jquery-2.0.0.min',
     isotope: '/jarvis/js/jquery-plugin/jquery.isotope.min',
     imagesloaded: '/jarvis/js/jquery-plugin/jquery.imagesloaded.min',
     jinja: '/jarvis/js/jinja.min',
-    bootstrap: 'http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min',
+    // bootstrap: 'http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min',
+    bootstrap: '/bootstrap/js/bootstrap.min',
 
     user: '/jarvis/js/user',
     share: '/jarvis/js/share',
